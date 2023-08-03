e69c0c873cdffba2e79a17acedcf673619e15cfc
nastya <anastasiia.sokolova.studies@gmail.com>
2023-08-03 09:09:25 +0200

    terraform code for 1 and 2

--- main.tf
+++ main.tf
@@ -4,14 +4,14 @@ provider "github" {
 }
 
 variable "token" {
-  type = string
+  type      = string
   sensitive = true
 }
 
 variable "repository_name" {
   description = "(Required) The name of the repository."
   type        = string
-  default = "github-terraform-task-anastasiia-sokolova1337"
+  default     = "github-terraform-task-anastasiia-sokolova1337"
 }
 
 resource "github_repository_collaborator" "collaborator" {
@@ -32,11 +32,30 @@ resource "github_branch_default" "this" {
   depends_on = [github_branch.develop]
 }
 
-resource "github_branch_protection" "this" {
-  pattern       = ""
-  repository_id = ""
+resource "github_branch_protection" "main" {
+  pattern       = "main"
+  repository_id = var.repository_name
+  required_pull_request_reviews {
+    require_code_owner_reviews = true
+  }
 }
 
+resource "github_branch_protection" "develop" {
+  pattern       = "develop"
+  repository_id = var.repository_name
+  required_pull_request_reviews {
+    required_approving_review_count = 2
+  }
+}
 
+resource "tls_private_key" "deploy_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
 
+resource "github_repository_deploy_key" "deploy_key" {
+  key        = tls_private_key.deploy_key.public_key_openssh
+  repository = var.repository_name
+  title      = "DEPLOY_KEY"
+}
 
